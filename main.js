4b145ff19c775e91fbaf434dc50a60a1ed0be4f2
Chema Torres <chemaaa94@gmail.com>
2022-06-01 18:46:19 -0500

    Mi primer API REST

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ console.log("Hello, World")
 
 const URL = "https://api.thedogapi.com/v1/images/search";
 
-fetch(URL)
-.then(res => res.json())
-.then(data => {
+async function myDog (){
+    const res = await fetch (URL);
+    const data = await res.json();
     const img = document.querySelector("img");
     img.src = data[0].url;
-});
+}
+
+const myButton = document.querySelector("button");
+myButton.onclick = myDog;
