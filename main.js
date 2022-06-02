dda8be8335a0a36f34f6cadaf40fb7fc42315c05
Chema Torres <chemaaa94@gmail.com>
2022-06-01 21:54:57 -0500

    Learning endpoint

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 console.log("Hello, World")
 
-const URL = "https://api.thedogapi.com/v1/images/search";
+const API_URL = "https://api.thedogapi.com/v1/images/search?limit=3";
 
 async function myDog (){
-    const res = await fetch (URL);
+    const res = await fetch (API_URL);
     const data = await res.json();
-    const img = document.querySelector("img");
-    img.src = data[0].url;
+    const img1 = document.getElementById("img1");
+    const img2 = document.getElementById("img2");
+    const img3 = document.getElementById("img3");
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
+    
+    
 }
 
 const myButton = document.querySelector("button");
 myButton.onclick = myDog;
+
+myDog()
