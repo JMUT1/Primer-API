54cef091aef1450436e06aa016db8e0f43435462
Chema Torres <chemaaa94@gmail.com>
2022-06-04 12:42:53 -0500

    learning GET & POST

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-console.log("Hello, World")
+const API_URL_RANDOM = "https://api.thedogapi.com/v1/images/search?limit=2&api_key=a748ab71-257b-45a7-90c9-b428252d9f12";
 
-const API_URL = "https://api.thedogapi.com/v1/images/search?limit=3";
+const API_URL_FAVORITES = "https://api.thedogapi.com/v1/favourites/search?limit=2&api_key=a748ab71-257b-45a7-90c9-b428252d9f12";
 
-async function reload (){
-    const res = await fetch (API_URL);
+const spanError = document.getElementById("error")
+
+
+async function loadRandomDogs (){
+    const res = await fetch (API_URL_RANDOM);
     const data = await res.json();
-    const img1 = document.getElementById("img1");
-    const img2 = document.getElementById("img2");
-    const img3 = document.getElementById("img3");
-    img1.src = data[0].url
-    img2.src = data[1].url
-    img3.src = data[2].url
-    
+
+    if(res.status !== 200){
+        spanError.innerHTML = "HUBO UN ERROR" + res.status
+
+    } else{
+        const img1 = document.getElementById("img1");
+        const img2 = document.getElementById("img2");
+        img1.src = data[0].url
+        img2.src = data[1].url
+    }
 }
 
-// const myButton = document.querySelector("button");
-// myButton.onclick = myDog;
+async function loadFavoritesDogs(){
+    const res = await fetch(API_URL_FAVORITES);
+    const data = await res.json()
+    console.log(data);
+}
 
-// myDog()
-reload()
+
+loadRandomDogs()
