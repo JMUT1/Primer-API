d5b2f4e52a64bb14d1b1353c44e58e0d04108a5e
Chema Torres <chemaaa94@gmail.com>
2022-06-04 12:03:51 -0500

    Maquetacion del proyecto

--- main.js
+++ main.js
@@ -2,20 +2,20 @@ console.log("Hello, World")
 
 const API_URL = "https://api.thedogapi.com/v1/images/search?limit=3";
 
-async function myDog (){
+async function reload (){
     const res = await fetch (API_URL);
     const data = await res.json();
     const img1 = document.getElementById("img1");
     const img2 = document.getElementById("img2");
     const img3 = document.getElementById("img3");
-    img1.src = data[0].url;
-    img2.src = data[1].url;
-    img3.src = data[2].url;
-    
+    img1.src = data[0].url
+    img2.src = data[1].url
+    img3.src = data[2].url
     
 }
 
-const myButton = document.querySelector("button");
-myButton.onclick = myDog;
+// const myButton = document.querySelector("button");
+// myButton.onclick = myDog;
 
-myDog()
+// myDog()
+reload()
